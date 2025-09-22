f332797a89fef35d13b817c44d2793c4095b5c97
paulinzz <paulofernandesalves30@gmail.com>
2025-09-21 22:02:05 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import AboutSection from "/home/paulinzz/Documents/my-portfolio/src/pages/home/Section/About/about.tsx"
-import HeroSection from "/home/paulinzz/Documents/my-portfolio/src/pages/home/Section/Hero/hero.tsx"
-import Navbar from "/home/paulinzz/Documents/my-portfolio/src/componets/NavBar/navbar.tsx"
-import ProjectsSection from "/home/paulinzz/Documents/my-portfolio/src/pages/home/Section/ProjectsSection/projectssection.tsx"
-import Footer from "/home/paulinzz/Documents/my-portfolio/src/componets/Footer/footer.tsx"
+import AboutSection from "src/pages/home/Section/About/about.tsx"
+import HeroSection from "src/pages/home/Section/Hero/hero.tsx"
+import Navbar from "src/componets/NavBar/navbar.tsx"
+import ProjectsSection from "src/pages/home/Section/ProjectsSection/projectssection.tsx"
+import Footer from "src/componets/Footer/footer.tsx"
 
 const App: React.FC = () => {
 
